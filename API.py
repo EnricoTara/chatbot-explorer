7cce19e65cd29dcc3aefe3d52fdb943fd3ee1c80
EnricoTara <Enricotara100@gmail.com>
2021-05-28 00:22:36 +0700

    Add files via upload

---
+++ API.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+
+from main import chatWithBot
+
+app = Flask(__name__)
+
+
+@app.route('/chat', methods=['GET', 'POST'])
+def chatBot():
+    chatInput = request.form['chatInput']
+    return jsonify(chatBotReply=get_response(chatInput))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
